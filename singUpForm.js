81717499576e97cdb5f55ee7b3af320bb91bc084
Konstantin Savchenko <killerkennyx41@gmail.com>
2019-07-19 12:16:30 +0300

    Same changes in Sing Up form and Footer

--- singUpForm.js
+++ singUpForm.js
@@ -5,9 +5,23 @@ $('select').styler({
 const singUpForCourseForm = document.querySelector('.singUpForCourse');
 
 singUpForCourseForm.addEventListener('click', (event) => {
-  console.log(event.target === document.querySelector('.fieldNameBeforeClick'));
-  if (event.target === 'label') {
+  if (event.target === document.querySelector('.fieldNameBeforeClick')) {
     event.target.classList.add('fieldNameAfterClick');
     event.target.classList.remove('fieldNameBeforeClick');
+  } else if (event.target === document.querySelector('input') || event.target === document.querySelector('textarea')) {
+    event.target.previousElementSibling.classList.add('fieldNameAfterClick');
+    event.target.previousElementSibling.classList.remove('fieldNameBeforeClick');
+  }
+});
+
+let currentAndPreviousFields = [];
+
+const formQuestionsContainerField = document.querySelector('.formQuestionsContainer');
+
+formQuestionsContainerField.addEventListener('click', (event) => {
+  if (event.target === document.querySelector('textarea')) {
+    event.target.previousElementSibling.classList.add('fieldNameAfterClick');
+    event.target.previousElementSibling.classList.remove('fieldNameBeforeClick');
+    event.target.style.lineHeight = '17px';
   }
 });
