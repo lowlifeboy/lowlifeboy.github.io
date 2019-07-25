2bb55a99cc2fc6a13385d537a181d93d1a44911f
Konstantin Savchenko <killerkennyx41@gmail.com>
2019-07-25 16:19:25 +0300

    Add adaptive home page

--- js/header.js
+++ js/header.js
@@ -1,3 +1,17 @@
 $(window).on("scroll touchmove", function () {
   $('#header').toggleClass('tiny', $(document).scrollTop() > 0);
 });
+
+document.querySelector('.menu').addEventListener('click', () => {
+  let navClasses = document.querySelector('nav').classList;
+  navClasses.contains('hide') 
+    ? navClasses.remove('hide')
+    : navClasses.add('hide');
+});
+
+// document.querySelector('body').addEventListener('click', () => {
+//   let navClasses = document.querySelector('nav').classList;
+//   if (!navClasses.contains('hide')) {
+//     navClasses.add('hide');
+//   }
+// });
